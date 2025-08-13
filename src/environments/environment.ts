8125f0472dfba26c3201c6aab6e438ddd0f17d7d
Kmilogirva <kmilogirva@gmail.com>
2025-08-12 21:09:08 -0500

    Add: Sidebar Ancho

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,6 +24,7 @@ export const environment = {
  obtenerUsuarioPorId: "User/obtenerusuarioporid",
  actualizarUsuarioPorId: "User/actualizarusuario",
  eliminarUsuarioPorId:"User/eliminarusuarioporid",
+ crearUsuario : "crearusuario",
 
 
 //Inicio Productos
